e588e881bfaca6e06d1df7746c8acb3fb50235aa
Shadow <Shadow@SHADOW-UULCQQOQ>
2022-08-21 23:48:24 +0200

    Agregar archivos de proyecto.

---
+++ AutoFFToggle/EventHandlers.cs
@@ -0,0 +1,25 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+
+namespace AutoFFToggle
+{
+    public class EventHandler
+	{
+		public void OnRoundStartEvent()
+		{
+            {
+                Server.FriendlyFire = false;
+            }
+        }
+
+		public void OnEndingRoundEvent(EndingRoundEventArgs ev)
+		{
+			if (ev.IsAllowed && ev.IsRoundEnded)
+			{
+                {
+                    Server.FriendlyFire = true;
+                }
+            }
+		}
+	}
+}
---
+++ AutoFFToggle/Plugin.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Exiled.API.Features;
+
+namespace AutoFFToggle
+{
+	public class AutoFFToggle : Plugin<Config>
+	{
+		public static AutoFFToggle Instance { get; set; }
+		public override string Name => nameof(AutoFFToggle);
+		public override string Author => "SandyJP";
+
+        public override Version Version => new Version(2, 3, 0);
+        public override Version RequiredExiledVersion => new Version(5, 3, 0);
+
+        public EventHandler Handler;
+
+        public AutoFFToggle()
+        {
+            Instance = this;
+        }
+
+		public override void OnEnabled()
+		{
+			if (Server.FriendlyFire)
+			{
+				Log.Info("Friendly Fire is already enabled on this server. AutoFFToggle will now be disabled.");
+				Config.IsEnabled = false;
+
+                return;
+			}
+
+            base.OnEnabled();
+			Log.Info("Plugin actualizado por SandyJP#1111");
+
+			Handler = new EventHandler();
+			Exiled.Events.Handlers.Server.RoundStarted += Handler.OnRoundStartEvent;
+			Exiled.Events.Handlers.Server.EndingRound += Handler.OnEndingRoundEvent;
+		}
+
+		public override void OnDisabled()
+		{
+			Exiled.Events.Handlers.Server.RoundStarted -= Handler.OnRoundStartEvent;
+			Exiled.Events.Handlers.Server.EndingRound -= Handler.OnEndingRoundEvent;
+
+            base.OnDisabled();
+			Handler = null;
+		}
+	}
+}
